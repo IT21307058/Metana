f725cb92ec3487d709beccd1bfc0f3c21fc8aa48
IT21307058 <IT21307058@my.sliit.lk>
2024-06-15 15:57:52 +0530

    changes commited

--- client/src/pages/Page5.js
+++ client/src/pages/Page5.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const languages = [
+  'Solidity', 'Rust', 'Node.js', 'Typescript', 'Javascript',
+  'C', 'C++', 'C#', 'SQL', 'Python', 'Assembly Language',
+  'Haskell', 'R', '.NET', 'Other'
+];
+
 
 function Page5() {
+  const [selectedLanguages, setSelectedLanguages] = useState([]);
+
+
   return (
-    <div>Page5</div>
+    <div className="page4">
+      {/* <div className="form"> */}
+        <p className="subtitle">Select all the languages you know.</p>
+        <div className="options-container">
+          {languages.map((language, index) => (
+            <label key={language} className={`option ${selectedLanguages.includes(language) ? 'selected' : ''}`} style={{ display: "flex", gap: "30px"}}>
+              [{String.fromCharCode(65 + index)}] {language}
+            </label>
+          ))}
+        </div>
+        <button type="submit" className="submit-button">OK</button>
+      {/* </div> */}
+    </div>
   )
 }
 
