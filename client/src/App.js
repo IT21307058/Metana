2cf760c12396a85b7a9b0f3985d064c7b05c4309
IT21307058 <IT21307058@my.sliit.lk>
2024-06-15 20:43:42 +0530

    add react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Form from "./pages/Form"
 import Home from './pages/Home';
+import { Routes as Switch, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <Form /> */}
-      <Home/>
+      {/* <Home/> */}
+      <Router>
+        <Switch>
+          <Route path='/' element={<Home />} />
+          <Route path='/buildform-test' element={<Form />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,43 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
-  return (
-    <div className="landing-page">
-      <div className="left-section">
-        <h1>Launch your Data Career in Weeks, not Years</h1>
-        <p className="expectations">What to expect:</p>
-        <ul className="expectations-list">
-          <li>Short-answer questions & No cover letter</li>
-          <li>Takes 4 mins on average</li>
-        </ul>
-        <button className="start-button">Start Your Journey</button>
-        <p className="submit-info">
-          <span role="img" aria-label="people">👥</span> 106 people have filled this out
-        </p>
-      </div>
-      <div className="right-section">
-        <div className="rating">
-          <span>Rated on</span>
-          <span className="course-report">Course Report</span>
-          <span className="rating-score">4.8</span>
+    const naviagte = useNavigate();
+    return (
+        <div className="landing-page">
+            <div className="left-section">
+                <h1>Launch your Data Career in Weeks, not Years</h1>
+                <p className="expectations">What to expect:</p>
+                <ul className="expectations-list">
+                    <li>Short-answer questions & No cover letter</li>
+                    <li>Takes 4 mins on average</li>
+                </ul>
+                <div style={{display:"flex", gap:"20px"}}>
+                    <button className="start-button" onClick={() => naviagte('/buildform-test')}>Start Your Journey</button>
+                    <p style={{color:"black"}}>press Enter ↵</p>
+                </div>
+                <p className="submit-info">
+                    <span role="img" aria-label="people">👥</span> 106 people have filled this out
+                </p>
+            </div>
+            <div className="right-section">
+                <div className="rating">
+                    <span>Rated on</span>
+                    <span className="course-report">Course Report</span>
+                    <span className="rating-score">4.8</span>
+                </div>
+                <div className="companies">
+                    <span>Our students used to work at</span>
+                    <div className="company-logos">
+                        <img src="" alt="Google" />
+                        <img src="" alt="IBM" />
+                        <img src="" alt="Nike" />
+                        <img src="" alt="eBay" />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="companies">
-          <span>Our students used to work at</span>
-          <div className="company-logos">
-            <img src="google-logo.png" alt="Google" />
-            <img src="ibm-logo.png" alt="IBM" />
-            <img src="nike-logo.png" alt="Nike" />
-            <img src="ebay-logo.png" alt="eBay" />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Home
